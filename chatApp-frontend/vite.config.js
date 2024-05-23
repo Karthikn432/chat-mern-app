53a7748bfff4e54e62c53792f27bdd267f3279ad
karthikn432 <karthicksekar432@gmail.com>
2024-05-23 16:10:01 +0530

    client-port-change

--- chatApp-frontend/vite.config.js
+++ chatApp-frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port : 5000,
+    port : 3000,
     proxy : {
       "/api":{
         target : "https://chat-mern-app-1lgm.onrender.com"
